136c514a243a3772eae99ed9f5139ed117078961
if <65443757+ifwwww@users.noreply.github.com>
2024-11-30 14:59:13 +0800

    Update unsplashConfig.js

--- src/utils/unsplashConfig.js
+++ src/utils/unsplashConfig.js
@@ -2,7 +2,7 @@ import { createApi } from 'unsplash-js';
 
 const key= process.env.REACT_APP_API_ACCESS_KEY
 const unsplash = createApi({
-    accessKey: key
+    accessKey: ksBcjGhchAC91a-ITUPrb6A3SWf2fmqFID7ilZCafKo
 });
 
 export default unsplash;
